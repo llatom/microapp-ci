306d6bc49ecebf71cc187a7902f71a86c5303665
czen <pealstyle@gmail.com>
2022-04-01 00:47:31 +0800

    feat: 执行upload, preview前先完成小程序构建

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+// /* eslint-disable no-console */
+import * as path from 'path'
+import fs from 'fs'
+const { spawn } = require('child_process')
 import WeappCI from './platform/weapp-ci'
 import TTCI from './platform/tt-ci'
 import AlipayCI from './platform/alipay-ci'
 import SwanCI from './platform/swan-ci'
 import { printLog } from './utils/printLog'
 import { DEPLOY_CONFIG_DATA } from './types/base-ci'
+
 
 export class MiniAppCi {
   constructor(private deployConfig: DEPLOY_CONFIG_DATA) {
@@ -37,17 +42,59 @@ export class MiniAppCi {
     }
   }
 
+  async build() {
+    const {env, platform} = this.deployConfig
+    const platformText = platform === 'weapp' ? '微信' : platform === 'alipay' ? '支付宝' : platform === 'swan' ? '百度' : '字节'
+    const outDir = this.deployConfig[platform].projectPath
+    printLog.pending(`正在编译${platformText}小程序...`)
+    const logFilePath = path.join(outDir, 'build_alipay.log')
+    const stream = fs.createWriteStream(logFilePath)
+    return new Promise<void>((resolve, reject) => {
+      const cmd = `taro build --type ${platform}`
+      const proc = spawn('npx', cmd.split(' '), {
+        env: {
+          ...process.env,
+          ...env
+        }
+      })
+      proc.stdout.on('data', data => {
+        stream.write(data)
+      })
+
+      proc.stderr.on('data', data => {
+        stream.write(data)
+      })
+
+      proc.on('error', (e) => {
+        console.error(`error: ${ e.message }`)
+        reject(e)
+      })
+
+      proc.on('close', code => {
+        if (code !== 0) {
+          printLog.error(`Failed building. See ${ logFilePath }`)
+          reject(`Exit code: ${ code }`)
+        } else {
+          printLog.success('编译完成')
+          resolve()
+        }
+      })
+    })
+  }
+
   async open() {
     const ci = this._init(this.deployConfig)
     ci.open()
   }
 
   async upload() {
+    await this.build()
     const ci = this._init(this.deployConfig)
     ci.upload()
   }
 
   async preview() {
+    await this.build()
     const ci = this._init(this.deployConfig)
     ci.preview()
   }
